004756eb2434d7ca79938f06c042847153bb8742
Sid <cidlibril5@gmail.com>
2023-06-24 16:12:05 +0800

    First commit

---
+++ schemas/userSchema.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    middleName: String,
+    password: {
+      type: String,
+      required: true,
+    },
+    schoolId: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    gender: {
+      type: String,
+    },
+    age: {
+      type: Number,
+      min: 1,
+      max: 100,
+    },
+    role: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      enum: ["admin", "student", "superadmin"],
+    },
+    status: {
+      type: String,
+      default: "pending",
+      trim: true,
+      required: true,
+      enum: ["pending", "approved", "rejected"],
+    },
+    recentAccess: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Exam",
+      }
+    ]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const cors = require('cors')
+require('dotenv').config();
+
+//setup
+const app = express();
+const port = process.env.PORT;
+const dbURI = process.env.ATLAS_URI;
+const urlencodedParser = bodyParser.urlencoded({extended:false})
+app.use(bodyParser.json(), urlencodedParser);
+app.use(cors());
+app.use(express.json());
+
+app.get('*', async function(req, res){
+  res.status(404).send({message:"URI does not exist"});
+});
+app.post('*', function(req, res){
+    res.status(404).send({message:"URI does not exist"});
+});
+app.put('*', function(req, res){
+    res.status(404).send({message:"URI does not exist"});
+});
+app.delete('*', function(req, res){
+    res.status(404).send({message:"URI does not exist"});
+});
+
+
+//Database
+mongoose.set("strictQuery", false);
+mongoose.connect(dbURI)
+    .then(() => {
+        console.log("Connected to the Database")
+    })
+    .catch((err) => {
+        console.log(err.message)
+    })
+app.listen(port, () => 
+    console.log("🔥Server is running on http:localhost:"+port)
+);

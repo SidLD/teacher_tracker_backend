9e97e69fd46063ee4c8b7bdaf83861cd9ec755f1
= <cidlibril5@gmail.com>
2023-09-08 20:42:26 +0800

    test

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json(), urlencodedParser);
 const corsOptions = {
     origin: "https://ohsp-student-tracker.onrender.com" // frontend URI (ReactJS)
 }
-app.use(cors(corsOptions));
+app.use(cors("*"));
 app.use(express.json());
 
 
